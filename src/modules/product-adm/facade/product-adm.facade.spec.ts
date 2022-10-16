1c5c787fe448ad4d0373d362dda6d7f5bf1353b7
Vyctor <dev.vyctor@gmail.com>
2022-10-16 00:00:48 -0300

    feat: testando facade para verificacao de inventario

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -1,9 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import { ProductAdmFacadeFactory } from "../factory/facade.factory";
 import { ProductModel } from "../repository/product.model";
-import { ProductRepository } from "../repository/product.repository";
-import { AddProductUsecase } from "../usecase/add-product/add-product.usecase";
-import { ProductAdmFacade } from "./product-adm.facade";
 
 describe("Product Adm Facade test", () => {
   let sequelize: Sequelize;
@@ -49,5 +46,44 @@ describe("Product Adm Facade test", () => {
     expect(product.description).toBe(input.description);
     expect(product.purchasePrice).toBe(input.purchasePrice);
     expect(product.inventory).toBe(input.inventory);
+  });
+
+  it("should check a product inventory", async () => {
+    const productFacade = ProductAdmFacadeFactory.create();
+
+    const input = {
+      id: "1",
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 10,
+      inventory: 100,
+    };
+
+    await productFacade.addProduct(input);
+
+    const product = await ProductModel.findOne({
+      where: {
+        id: input.id,
+      },
+    });
+
+    expect(product).toBeDefined();
+    expect(product.id).toBe(input.id);
+    expect(product.name).toBe(input.name);
+    expect(product.description).toBe(input.description);
+    expect(product.purchasePrice).toBe(input.purchasePrice);
+    expect(product.inventory).toBe(input.inventory);
+
+    const checkInventoryInput = {
+      productId: input.id,
+    };
+
+    const checkInventoryOutput = await productFacade.checkInventory(
+      checkInventoryInput
+    );
+
+    expect(checkInventoryOutput).toBeDefined();
+    expect(checkInventoryOutput.productId).toBe(input.id);
+    expect(checkInventoryOutput.inventory).toBe(input.inventory);
   });
 });
