2434ad84867fe30e290cbb277cbc6adac4f9f5d3
Vyctor <dev.vyctor@gmail.com>
2022-10-29 19:10:32 -0300

    iniciando com payment

---
+++ src/modules/payment/domain/transaction.ts
@@ -0,0 +1,41 @@
+import { Id } from "../../@shared/domain/value-object/id.value-object";
+import { BaseEntity } from "../../@shared/domain/entity/base.entity";
+import AggregateRoot from "../../@shared/domain/entity/aggregate-root.interface";
+
+type TransactionProps = {
+  id?: Id;
+  amount: number;
+  orderId: string;
+  status?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export class Transaction extends BaseEntity implements AggregateRoot {
+  private _amount: number;
+  private _orderId: string;
+  private _status: string;
+
+  constructor(props: TransactionProps) {
+    super(props.id, props.createdAt, props.updatedAt);
+    this._amount = props.amount;
+    this._orderId = props.orderId;
+    this._status = props.status || "pending";
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get orderId() {
+    return this._orderId;
+  }
+
+  get status() {
+    return this._status;
+  }
+
+  set status(status: string) {
+    this._status = status;
+  }
+}
