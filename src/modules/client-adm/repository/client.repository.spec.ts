2b56568e656d8e2f9fc0fdf7645f90cae5d36ab5
Vyctor <dev.vyctor@gmail.com>
2022-10-29 17:52:54 -0300

    implementando metodo find no ClientRepository

--- src/modules/client-adm/repository/client.repository.spec.ts
+++ src/modules/client-adm/repository/client.repository.spec.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import { ClientModel } from "./client.model";
+import ClientRepository from "./client.repository";
 
 describe("Client Repository test", () => {
   let sequelize: Sequelize;
@@ -18,5 +19,27 @@ describe("Client Repository test", () => {
 
   afterEach(async () => {
     await sequelize.close();
+  });
+
+  it("should find a client", async () => {
+    const client = await ClientModel.create({
+      id: "1",
+      name: "John Doe",
+      email: "client@email.com",
+      address: "Street 1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const repository = new ClientRepository();
+
+    const result = await repository.find(client.id);
+
+    expect(result.id.id).toEqual(client.id);
+    expect(result.name).toEqual(client.name);
+    expect(result.email).toEqual(client.email);
+    expect(result.address).toEqual(client.address);
+    expect(result.createdAt).toEqual(client.createdAt);
+    expect(result.updatedAt).toEqual(client.updatedAt);
   });
 });
