f5d34be8d3e239767ec4af58c70338ad03eefc81
Vyctor <dev.vyctor@gmail.com>
2022-10-29 22:14:49 -0300

    finalizando testes do caso de uso

--- src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
+++ src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
@@ -2,22 +2,35 @@ import { Id } from "../../../@shared/domain/value-object/id.value-object";
 import { Transaction } from "../../domain/transaction";
 import { ProcessPaymentUsecase } from "./process-payment.usecase";
 
-const transaction = new Transaction({
+const transactionApprovedMock = new Transaction({
   id: new Id("1"),
   amount: 100,
   orderId: "1",
   status: "approved",
 });
 
-const MockRepository = () => {
+const transactionDeclinedMock = new Transaction({
+  id: new Id("1"),
+  amount: 50,
+  orderId: "1",
+  status: "declined",
+});
+
+const MockRepositoryApproved = () => {
   return {
-    save: jest.fn().mockResolvedValue(transaction),
+    save: jest.fn().mockResolvedValue(transactionApprovedMock),
+  };
+};
+
+const MockRepositoryDeclined = () => {
+  return {
+    save: jest.fn().mockResolvedValue(transactionDeclinedMock),
   };
 };
 
 describe("Process Payment Usecase unit test", () => {
   it("should process payment", async () => {
-    const paymentRepository = MockRepository();
+    const paymentRepository = MockRepositoryApproved();
     const usecase = new ProcessPaymentUsecase(paymentRepository);
     const input = {
       orderId: "1",
@@ -29,8 +42,23 @@ describe("Process Payment Usecase unit test", () => {
     expect(result).toBeDefined();
     expect(result.status).toBe("approved");
     expect(paymentRepository.save).toHaveBeenCalled();
-    expect(result.transactionId).toBe(transaction.id.id);
+    expect(result.transactionId).toBe(transactionApprovedMock.id.id);
     expect(result.orderId).toBe(input.orderId);
     expect(result.amount).toBe(input.amount);
+  });
+
+  it("should decline a transaction", async () => {
+    const paymentRepository = MockRepositoryDeclined();
+    const usecase = new ProcessPaymentUsecase(paymentRepository);
+    const input = {
+      orderId: "1",
+      amount: 50,
+    };
+
+    const result = await usecase.execute(input);
+
+    expect(paymentRepository.save).toHaveBeenCalled();
+    expect(result.status).toBe("declined");
+    expect(result.amount).toBe(50);
   });
 });
